e7a3be5d679505fe91dae3ac5828f4433004eb28
Marcin Dzierwa <vientvotredouleur@gmail.com>
2023-07-14 18:10:16 +0200

    fixed models sometimes changing between form submissions - binded models to a variable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,7 @@
 	export let data: PageData;
 
 	let stepsValue = 10;
+	let currentModel: string;
 </script>
 
 <Container>
@@ -28,6 +29,7 @@
 							Select a model
 						</label>
 						<select
+							bind:value={currentModel}
 							id="sd-model"
 							name="sd-model"
 							class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
